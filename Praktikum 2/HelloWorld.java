e5caf2cbb22a0eae5b678d8a8430a5287c801361
unknown <siffzaen@student.telkomuniversity.ac.id>
2020-09-17 10:11:13 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+    public class HelloWorld {
+		
+		public static void main(String[] args) {
+			System.out.println("Nama : Siffa Zaena");
+			System.out.println("Nim : 6706204081");
+			System.out.println("Kelas : D3IF 44-04");
+		}
+	}
